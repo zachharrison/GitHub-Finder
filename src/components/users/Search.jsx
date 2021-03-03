e09d242f5211d678f1e598d203626337ac4ed8f7
Zach Harrison <zharrison@shaw.ca>
2021-03-03 11:36:01 -0800

    Completed core functionality

---
+++ src/components/users/Search.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
+  const [text, setText] = useState('')
+
+  const onChange = (e) => setText(e.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if(text === '') {
+      setAlert('Please enter some text', 'light');
+    } else {
+      searchUsers(text);
+      setText('');
+    }
+  }
+
+  return (
+    <div>
+      <form  onSubmit={onSubmit} action="" className="form">
+        <input 
+          type="text" 
+          name="text" 
+          autoComplete="off" 
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input 
+          type="submit" 
+          value="Search" 
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {
+        showClear && 
+        <button 
+          className="btn btn-light btn-block" 
+          onClick={clearUsers}>
+          Clear
+        </button>
+      }
+    </div>
+  )
+}
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default Search
