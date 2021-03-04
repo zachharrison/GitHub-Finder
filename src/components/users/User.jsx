40391d19a49f9488885668622a2c5cbded17d9c5
Zach Harrison <zharrison@shaw.ca>
2021-03-03 16:15:10 -0800

    Successfully refactored repos state to use context

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -5,10 +5,10 @@ import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import GithubContext from '../../context/github/githubContext';
 
-const User = ({ getUserRepos, repos, match }) => {
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext);
 
-  const { getUser, loading, user } = githubContext;
+  const { getUser, loading, user, repos, getUserRepos } = githubContext;
 
   useEffect(() => {
     getUser(match.params.login);
@@ -105,11 +105,6 @@ const User = ({ getUserRepos, repos, match }) => {
       <Repos repos={repos} />     
     </Fragment>
   )
-}
-
-User.propTypes = {
-  repos: PropTypes.array.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
 }
 
 export default User
--- src/context/github/GithubState.jsx
+++ src/context/github/GithubState.jsx
@@ -44,7 +44,17 @@ const GithubState = (props) => {
     })
   };
 
-  // GET REPOS
+  // GET USERS REPOS
+  const getUserRepos = async (username) => {
+    setLoading();
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    })
+  };
 
   // CLEAR USERS FROM STATE
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
@@ -62,6 +72,7 @@ const GithubState = (props) => {
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {props.children}
